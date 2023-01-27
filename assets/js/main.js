2fe8590d085e50e3d80c1eb4720a3aa4a80766dd
karinlee <karinlee92@gmail.com>
2023-01-27 12:40:05 +0100

    added javascript functions

---
+++ assets/js/main.js
@@ -0,0 +1,138 @@
+//Variables Input
+const yearList = document.querySelector("#yearList");
+const single = document.querySelector("#single");
+const splitting = document.querySelector("#splitting");
+const einkommen = document.querySelector("#einkommen");
+
+console.log(yearList, single, splitting, einkommen);
+
+//Output
+
+const zvE = document.querySelector("#zvE");
+const ESt = document.querySelector("#ESt");
+
+console.log(zvE, ESt);
+
+//function
+
+const calculate =() => {
+    let input = Number(einkommen.value);
+    let output1 ="";
+    let y = "";
+    let output2 ="";
+    
+    if(single.checked){
+        output1 = input;
+    } else{
+        output1 = input / 2;
+    }
+console.log('test');
+    const year2020 = (output1) =>{
+        switch(true){
+        
+            case (output1 <= 9408):
+                output2 = 0;
+                console.log(output2);
+            break;
+        
+            case (output1 >= 9408 && output1 <= 14532):
+            y = (output1 - 9408)/10000;
+            output2 = (972.87 * y + 1400) * y;
+            console.log(output2);
+            break;
+        
+            case (output1 >= 14533 && output1 <= 57051):
+            y = (output1 - 14532)/10000;
+            output2 = (212.02 * y + 2397) * y + 972.79;
+            break;
+        
+            case (output1 >= 57052 && output1 <= 270500):
+                output2= 0.42 * output1 - 8963.74;
+            break;
+        
+            case(output1 >= 270501):
+            output2 = 0.45 * output1 - 17078.74;
+            break;
+        
+        }
+    }
+
+    const year2021 = (output1) =>{
+        switch(true){
+        
+            case (output1 <= 9744):
+                output2 = 0;
+            break;
+        
+            case (output1 >= 9745 && output1 <= 14753):
+            y = (output1 - 9744)/10000;
+            output2 = (995.21 * y + 1400) * y;
+            break;
+        
+            case (output1 >= 14754 && output1 <= 57918):
+            y = (output1 - 14753)/10000;
+            output2 = (208.85 * y + 2397) * y + 950.96;
+            break;
+        
+            case (output1 >= 57919 && output1 <= 274612):
+                output2= 0.42 * output1 - 9136.63;
+            break;
+        
+            case(output1 >= 274613):
+            output2 = 0.45 * output1 - 17374.99;
+            break;
+        
+        }
+    }
+
+    const year2022 = (output1) =>{
+
+        switch(true){
+        
+            case (output1 <= 10347):
+            output2 = 0;
+            break;
+        
+            case (output1 >= 10348 && output1 <= 14926):
+            y = (output1 - 10347)/10000;
+            output2 = (1088.67 * y + 1400) * y;
+            break;
+        
+            case (output1 >= 14927 && output1 <= 58596):
+            y = (output1 - 14926)/10000;
+            output2 = (206.43 * y + 2397) * y + 869.32;
+            break;
+        
+            case (output1 >= 58597 && output1 <= 277825):
+                output2= 0.42 * output1 - 9336.45;
+            break;
+        
+            case(output1 >= 277826):
+            output2 = 0.45 * output1 - 17671.20;
+            break;
+        
+        }
+    
+    }
+
+    switch(yearList.value){
+        case "2":
+            year2020(output1);
+        break;
+        case "1":
+            year2021(output1);
+        break;
+        default:
+            year2022(output1);
+    }
+console.log('test2');
+    let result = Math.floor(output2);
+    console.log(result);
+    let result2 = Math.round(result);
+    console.log(result2);
+
+    zvE.innerHTML = output1.toFixed(2);
+    ESt.innerHTML = result2.toFixed(2);
+
+}
+
