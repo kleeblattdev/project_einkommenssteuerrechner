4c379408136af4381a06c08a7d5845cf6542b22e
karinlee <karinlee92@gmail.com>
2023-01-27 13:48:51 +0100

    added styling and minor changes in html and javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,49 +17,45 @@ console.log(zvE, ESt);
 
 const calculate =() => {
     let input = Number(einkommen.value);
-    let output1 ="";
-    let y = "";
-    let output2 ="";
+    let output1 = ""; // zvE
+    let y = ""; // Zwischenschritt für Berechnung der ESt
+    let output2 =""; // errechnete ESt
     
+    //Art der Veranlagung 
     if(single.checked){
         output1 = input;
     } else{
         output1 = input / 2;
     }
-console.log('test');
+
+    //Funktionen für das Jahr 2020
     const year2020 = (output1) =>{
         switch(true){
-        
             case (output1 <= 9408):
                 output2 = 0;
                 console.log(output2);
             break;
-        
             case (output1 >= 9408 && output1 <= 14532):
             y = (output1 - 9408)/10000;
             output2 = (972.87 * y + 1400) * y;
             console.log(output2);
             break;
-        
             case (output1 >= 14533 && output1 <= 57051):
             y = (output1 - 14532)/10000;
             output2 = (212.02 * y + 2397) * y + 972.79;
             break;
-        
             case (output1 >= 57052 && output1 <= 270500):
                 output2= 0.42 * output1 - 8963.74;
             break;
-        
             case(output1 >= 270501):
             output2 = 0.45 * output1 - 17078.74;
             break;
-        
         }
     }
 
+    //Funktionen für das Jahr 2021
     const year2021 = (output1) =>{
         switch(true){
-        
             case (output1 <= 9744):
                 output2 = 0;
             break;
@@ -81,12 +77,11 @@ console.log('test');
             case(output1 >= 274613):
             output2 = 0.45 * output1 - 17374.99;
             break;
-        
         }
     }
 
+    //Funktionen für das Jahr 2022
     const year2022 = (output1) =>{
-
         switch(true){
         
             case (output1 <= 10347):
@@ -110,11 +105,9 @@ console.log('test');
             case(output1 >= 277826):
             output2 = 0.45 * output1 - 17671.20;
             break;
-        
         }
-    
     }
-
+    //Welches Jahr wurde ausgewählt
     switch(yearList.value){
         case "2":
             year2020(output1);
@@ -122,17 +115,23 @@ console.log('test');
         case "1":
             year2021(output1);
         break;
-        default:
+        case "0":
             year2022(output1);
+        break;
     }
-console.log('test2');
-    let result = Math.floor(output2);
-    console.log(result);
-    let result2 = Math.round(result);
-    console.log(result2);
 
-    zvE.innerHTML = output1.toFixed(2);
-    ESt.innerHTML = result2.toFixed(2);
+    let result = Math.floor(output2); //Abrunden
+    let result2 = Math.round(result); //Ergebnis als ganze Zahl
+
+    //Ausgabe mit 2 Nachkommastellen
+    if (splitting.checked){
+        zvE.innerHTML = output1.toFixed(2) + ` € p.P.`; 
+        ESt.innerHTML = result2.toFixed(2) + ` € p.P.`;
+    }
+    else{
+        zvE.innerHTML = output1.toFixed(2) + ` €`; 
+        ESt.innerHTML = result2.toFixed(2) + ` €`;
+    }
 
 }
 
